a2e0d851ec4118a05d4964d521060aa5e6930348
jackytank <balisongian@gmail.com>
2023-06-08 07:15:53 +0700

    update

---
+++ mslearn-dotnet-files/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesToDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesToDir);
+
+var salesFiles = FindFiles(storesDirectory);
+
+File.WriteAllText(Path.Combine(salesToDir, "totals.txt"), String.Empty);
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> files = new List<string>();
+    var foundFiles = Directory.EnumerateFiles("stores", "*.json", SearchOption.AllDirectories);
+    foreach (var item in foundFiles)
+    {
+        var extention = Path.GetExtension(item);
+        if (extention == ".json")
+        {
+            files.Add(item);
+        }
+    }
+    return files;
+}
+
+record SalesData(double Total);
